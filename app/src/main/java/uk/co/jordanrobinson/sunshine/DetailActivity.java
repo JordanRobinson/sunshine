db4154db646c11143d05c99317ad035cb1b7f1d1
Jordan Robinson <hypahnova@gmail.com>
2015-02-16 23:58:56 +0000

    cleaning up a little

--- app/src/main/java/uk/co/jordanrobinson/sunshine/DetailActivity.java
+++ app/src/main/java/uk/co/jordanrobinson/sunshine/DetailActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
 public class DetailActivity extends ActionBarActivity {
 
     @Override
@@ -28,7 +27,6 @@ public class DetailActivity extends ActionBarActivity {
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
